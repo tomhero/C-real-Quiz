b94369089f937a599f5d704f7c0f607b37e20170
Chayut <s7070026@kmitl.ac.th>
2015-03-26 21:22:05 +0700

    include test.c
    This is a test program.

---
+++ test.c
@@ -0,0 +1,36 @@
+#include <gtk/gtk.h>
+#include <stdio.h>
+
+static void  but_clk(GtkWidget* widget, gpointer data)
+{
+    gtk_label_set_text(GTK_LABEL(data), "Passed");
+    printf("stdclick\n");
+}
+
+int main (int   argc, char *argv[])
+{
+  GtkWidget *window, *label, *button, *vbox;
+
+  gtk_init (&argc, &argv);
+
+  window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+  gtk_window_set_title (GTK_WINDOW (window), "TEST");
+  label = gtk_label_new("Hello World");
+  button = gtk_button_new_with_label("Click Me!!");
+  vbox = gtk_vbox_new(0, 0);
+  gtk_box_pack_start(GTK_BOX(vbox), label, 1, 1, 0);
+  gtk_box_pack_start(GTK_BOX(vbox), button, 1, 1, 50);
+
+  g_signal_connect(window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
+  g_signal_connect(button, "clicked", G_CALLBACK(but_clk), label);
+  //gtk_container_add(GTK_CONTAINER(window), label);
+  gtk_container_add(GTK_CONTAINER(window), vbox);
+  gtk_widget_set_size_request(window, 300, 300);
+  gtk_window_set_title(GTK_WINDOW(window), "TEST");
+
+  gtk_widget_show_all(window);
+
+  gtk_main ();
+
+  return 0;
+}
