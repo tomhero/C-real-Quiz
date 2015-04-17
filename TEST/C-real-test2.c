6a66689c5172195db4ea8b04d998ddee605de928
Chayut <s7070026@kmitl.ac.th>
2015-04-17 19:09:02 +0700

    fix some bug
    a little BUG

--- TEST/C-real-test2.c
+++ TEST/C-real-test2.c
@@ -11,20 +11,16 @@ int doing_quiz_even();
 int update();
 int choose_quiz(GtkWidget *widget, gpointer bigbox);
 int named(GtkWidget *widget, gpointer data);
+int about();
+int rule();
+int show_credit();
 
 // Global variable
 GtkWidget *main_menu, *count_down, *quiz_window, *true_choice, *false_choice, *timer;
 GtkEntry *entry;
 char player_name[50] , str[2];
-int set = 0, num_quiz = 0, round = 3, score = 0, sign = 0;
+int set = 0, num_quiz = 0, round = 3, score = 0, sign = 9, alert = 0;
 float percent = 0.0;
-
-
-int window_destroy(GtkWidget *widget, gpointer data)
-{
-    gtk_widget_destroy(data);
-    exit(0);
-}
 
 int widget_destroy(GtkWidget *widget, gpointer data)
 {
@@ -97,43 +93,53 @@ int random()
 int score_board()
 {
     GtkWidget *bigbox, *vbox_up, *vbox_down, *hbox, *but_home, *but_exit, *player, *label, *score_label;
-    char last_score[2];
+    GtkImage *pic_your_score, *pic_replay, *pic_exit;
+    char last_score[2], player_label[100] = "Player name \" ", label_score[20] = ">> ";
     printf("passed!!\n");
     /*----- CSS ----------- */
     GtkCssProvider *provider;
     GdkDisplay *display;
     GdkScreen *screen;
     /*-----------------------*/
-    if (sign <= 9)
+    if (sign > alert)
     {
-        sign += 1;
+        sign -= 1;
         return 0;
     }
     gtk_widget_destroy(main_menu);
     main_menu = gtk_window_new (GTK_WINDOW_TOPLEVEL);
     gtk_window_set_title (GTK_WINDOW (main_menu), "C-real Quiz");
-    player = gtk_label_new(player_name);
-    label = gtk_label_new("You score!");
+    strcat(player_label, player_name);
+    strcat(player_label, " \"");
+    player = gtk_label_new(player_label);
+    pic_your_score = gtk_image_new_from_file("button/score.png");
     sprintf(last_score, "%d", score);
-    score_label = gtk_label_new(last_score);
-    but_home = gtk_button_new_with_label("REPLAY");
-    but_exit = gtk_button_new_with_label("EXIT");
+    strcat(label_score,  last_score);
+    strcat(label_score,  " <<");
+    score_label = gtk_label_new(label_score);
+    but_home = gtk_button_new();
+    but_exit = gtk_button_new();
+    pic_replay = gtk_image_new_from_file("button/replay.png");
+    pic_exit = gtk_image_new_from_file("exit.png");
+    gtk_button_set_image(but_exit, pic_exit);
+    gtk_button_set_image(but_home, pic_replay);
     bigbox = gtk_vbox_new(0, 0);
     vbox_up = gtk_vbox_new(0, 0);
     vbox_down = gtk_vbox_new(0, 0);
     hbox = gtk_hbox_new(0, 0);
+    gtk_widget_set_name(score_label, "score");
     gtk_widget_set_name(main_menu, "main");
     gtk_widget_set_name(player, "player");
-    gtk_box_pack_start(GTK_BOX(bigbox), vbox_up, 1, 1, 10);
-    gtk_box_pack_start(GTK_BOX(bigbox), vbox_down, 1, 1, 10);
-    gtk_box_pack_start(GTK_BOX(vbox_up), player, 1, 1, 50);
-    gtk_box_pack_start(GTK_BOX(vbox_up), label, 1, 1, 50);
-    gtk_box_pack_start(GTK_BOX(vbox_up), score_label, 1, 1, 0);
+    gtk_box_pack_start(GTK_BOX(bigbox), vbox_up, 1, 1, 25);
+    gtk_box_pack_start(GTK_BOX(bigbox), vbox_down, 1, 1, 0);
+    gtk_box_pack_start(GTK_BOX(vbox_up), player, 1, 1, 10);
+    gtk_box_pack_start(GTK_BOX(vbox_up), pic_your_score, 1, 1, 20);
+    gtk_box_pack_start(GTK_BOX(vbox_up), score_label, 1, 1, 25);
     gtk_box_pack_start(GTK_BOX(vbox_down), hbox, 1, 1, 0);
-    gtk_box_pack_start(GTK_BOX(hbox), but_home, 1, 1, 50);
-    gtk_box_pack_start(GTK_BOX(hbox), but_exit, 1, 1, 50);
+    gtk_box_pack_start(GTK_BOX(hbox), but_home, 1, 1, 100);
+    gtk_box_pack_start(GTK_BOX(hbox), but_exit, 1, 1, 100);
     gtk_container_add(GTK_CONTAINER(main_menu), bigbox);
-    g_signal_connect(but_exit, "clicked", G_CALLBACK(window_destroy), main_menu);
+    g_signal_connect(but_exit, "clicked", G_CALLBACK(gtk_main_quit), NULL);
     g_signal_connect(but_home, "clicked", G_CALLBACK(named), bigbox);
     g_signal_connect(but_home, "clicked", G_CALLBACK(widget_destroy), vbox_up);
     g_signal_connect(but_home, "clicked", G_CALLBACK(widget_destroy), vbox_down);
@@ -150,9 +156,13 @@ int score_board()
                                      " #main {\n"
                                      "   background-image: url('background.jpg');\n"
                                      "   background-size: 800px 600px;\n"
+                                     "   background-repeat: no-repeat;\n"
                                      "}\n"
                                      " #player {\n"
                                      " font: 25\n"
+                                     "}\n"
+                                     " #score {\n"
+                                     " font: 60\n"
                                      "}\n",
                                      -1, NULL);
   g_object_unref (provider);
@@ -167,16 +177,18 @@ int score_board()
 
 int timer_quiz(gpointer data)
 {
-    percent -= 0.00008;
-    if(percent <= 0.0)
+    percent -= 0.00009;
+    if (percent <= 0.0)
     {
         gtk_widget_destroy(quiz_window);
         if (num_quiz % 2 == 0 && num_quiz < 10)
         {
+            alert += 1;
             return doing_quiz_even();
         }
         else if (num_quiz < 10)
         {
+            alert += 1;
             return doing_quiz_odd();
         }
         else
@@ -288,6 +300,7 @@ int doing_quiz_even()
                                      " #quiz_main {\n"
                                      "   background-image :url('background.jpg');\n"
                                      "   background-size: 800px 600px;\n"
+                                     "   background-repeat: no-repeat;\n"
                                      "}\n"
                                      " #true_choice {\n"
                                      " border-color: green;\n"
@@ -391,6 +404,7 @@ int doing_quiz_odd()
                                      " #quiz_main {\n"
                                      "   background-image :url('background.jpg');\n"
                                      "   background-size: 800px 600px;\n"
+                                     "   background-repeat: no-repeat;\n"
                                      "}\n"
                                      " #true_choice {\n"
                                      " border-color: green;\n"
@@ -433,23 +447,47 @@ int inc_progress_pre(gpointer data)
 
 int pre_do(GtkWidget *widget, gpointer bigbox)
 {
-    GtkWidget *progress, *progress2, *vbox, *ready;
+    GtkWidget *progress, *progress2, *vbox;
+    GtkImage *ready;
+    /*----- CSS ----------- */
+    GtkCssProvider *provider;
+    GdkDisplay *display;
+    GdkScreen *screen;
+    /*-----------------------*/
     sprintf(str, "%d", round);
     progress = gtk_progress_bar_new();
     vbox = gtk_vbox_new(0, 0);
-    ready = gtk_label_new("Are you ready?");
+    ready = gtk_image_new_from_file("button/ready.png");
     count_down = gtk_label_new(str);
     gtk_box_pack_start(GTK_BOX(bigbox), vbox, 1, 1, 5);
     gtk_box_pack_start(GTK_BOX(vbox), ready, 1, 1, 0);
-    gtk_box_pack_start(GTK_BOX(vbox), progress, 1, 1, 5);
+    gtk_box_pack_start(GTK_BOX(vbox), progress, 1, 1, 10);
     gtk_box_pack_start(GTK_BOX(vbox), count_down, 1, 1, 25);
+    gtk_widget_set_name(count_down, "count");
 
 //      gtk_progress_bar_set_text(GTK_PROGRESS_BAR(progress), "50%");
 
 //      gtk_progress_bar_set_orientation(GTK_PROGRESS_BAR(progress), GTK_PROGRESS_TOP_TO_BOTTOM);
 
+/*------------- CSS  --------------------------------------------------------------------------------------------------*/
+  provider = gtk_css_provider_new ();
+  display = gdk_display_get_default ();
+  screen = gdk_display_get_default_screen (display);
+
+  gtk_style_context_add_provider_for_screen (screen,
+                                             GTK_STYLE_PROVIDER(provider),
+                                             GTK_STYLE_PROVIDER_PRIORITY_USER);
+
+  gtk_css_provider_load_from_data (GTK_CSS_PROVIDER (provider),
+                                     " #count {\n"
+                                     " font: 60\n"
+                                     "}\n",
+                                     -1, NULL);
+  g_object_unref (provider);
+/*----------------------------------------------------------------------------------------------------------------------*/
+
     g_timeout_add(1, inc_progress_pre, progress);
-    gtk_container_set_border_width (GTK_CONTAINER (main_menu), 150);
+    gtk_container_set_border_width (GTK_CONTAINER (main_menu), 125);
     gtk_widget_show_all(bigbox);
 }
 
@@ -460,7 +498,7 @@ int choose_quiz(GtkWidget *widget, gpointer bigbox)
     GtkImage *img1, *img2, *img3, *img4, *img5, *img6, *label;
     strcpy(player_name, gtk_entry_get_text(entry));
     printf("%s\n", player_name);
-    label = gtk_image_new_from_file("choose_set/select-quiz.gif");
+    label = gtk_image_new_from_file("choose_set/select-quiz.png");
     choice_1 = gtk_button_new();
     choice_2 = gtk_button_new();
     choice_3 = gtk_button_new();
@@ -537,21 +575,25 @@ int choose_quiz(GtkWidget *widget, gpointer bigbox)
 
 int named(GtkWidget *widget, gpointer data)
 {
-    GtkWidget *window, *label, *but_ok, *vbox, *vbox_2, *choice_2, *layout, *image;
+    GtkWidget *window, *label, *but_ok, *vbox, *vbox_2, *choice_2, *layout;
+    GtkImage *enter, *pic_ok;
     set = 0;
     num_quiz = 0;
     round = 3;
     score = 0;
-    sign = 0;
-    label = gtk_label_new("Enter Your name");
-    but_ok = gtk_button_new_with_label("OK");
+    sign = 9;
+    alert = 0;
+    label = gtk_image_new_from_file("button/name.png");
+    but_ok = gtk_button_new();
+    pic_ok = gtk_image_new_from_file("button/ok.png");
+    gtk_button_set_image(but_ok,pic_ok);
     entry = gtk_entry_new();
     vbox_2 = gtk_vbox_new(0, 5);
     gtk_box_pack_start(GTK_BOX(data), vbox_2, 1, 1, 0);
     gtk_box_pack_start(GTK_BOX(vbox_2), label, 1, 1, 20);
     gtk_box_pack_start(GTK_BOX(vbox_2), entry, 1, 1, 0);
     gtk_box_pack_start(GTK_BOX(vbox_2), but_ok, 1, 1, 0);
-    gtk_container_set_border_width (GTK_CONTAINER (main_menu), 200);
+    gtk_container_set_border_width (GTK_CONTAINER (main_menu), 180);
     g_signal_connect(but_ok, "clicked", G_CALLBACK(choose_quiz), data);
     g_signal_connect(but_ok, "clicked", G_CALLBACK(widget_destroy), vbox_2);
     gtk_widget_show_all(data);
@@ -560,7 +602,7 @@ int named(GtkWidget *widget, gpointer data)
 int main (int argc, char *argv[])
 {
     GtkWidget *but_start, *but_exit, *but_about, *boxin, *boxout, *bigbox, *doneleft, *doneright, *label, *label2, *label3, *first, *credit;
-    GtkImage *logo;
+    GtkImage *logo, *pic_start, *pic_about, *pic_credit, *pic_exit;
     /*----- CSS ----------- */
     GtkCssProvider *provider;
     GdkDisplay *display;
@@ -570,10 +612,18 @@ int main (int argc, char *argv[])
     main_menu = gtk_window_new (GTK_WINDOW_TOPLEVEL);
     gtk_window_set_title (GTK_WINDOW (main_menu), "C-real Quiz");
     logo = gtk_image_new_from_file("logo.png");
-    but_start = gtk_button_new_with_label("Start");
-    but_about = gtk_button_new_with_label("About");
-    but_exit = gtk_button_new_with_label("Exit");
-    credit = gtk_button_new_with_label("Credit");
+    but_start = gtk_button_new();
+    but_about = gtk_button_new();
+    but_exit = gtk_button_new();
+    credit = gtk_button_new();
+    pic_start = gtk_image_new_from_file("button/start.png");
+    pic_about = gtk_image_new_from_file("button/about.png");
+    pic_credit = gtk_image_new_from_file("button/credit.png");
+    pic_exit = gtk_image_new_from_file("button/exit.png");
+    gtk_button_set_image(but_start,pic_start);
+    gtk_button_set_image(but_about,pic_about);
+    gtk_button_set_image(but_exit,pic_exit);
+    gtk_button_set_image(credit,pic_credit);
     label = gtk_label_new("");
     label2 = gtk_label_new("");
     label3 = gtk_label_new("");
@@ -585,23 +635,25 @@ int main (int argc, char *argv[])
     first = gtk_vbox_new(0, 0);
     gtk_box_pack_start(GTK_BOX(first), bigbox, 1, 1, 0);
     gtk_box_pack_start(GTK_BOX(bigbox), doneleft, 1, 1, 0);
-    gtk_box_pack_start(GTK_BOX(doneleft), label, 1, 1, 250);
+    gtk_box_pack_start(GTK_BOX(doneleft), label, 1, 1, 240);
     gtk_box_pack_start(GTK_BOX(doneleft), credit, 1, 1, 0);
     gtk_box_pack_start(GTK_BOX(bigbox), boxout, 1, 1, 0);
     gtk_box_pack_start(GTK_BOX(boxout), boxin, 1, 1, 0);
     gtk_box_pack_start(GTK_BOX(boxin), logo, 0, 0, 5);
-    gtk_box_pack_start(GTK_BOX(boxin), but_start, 1, 1, 20);
+    gtk_box_pack_start(GTK_BOX(boxin), but_start, 1, 1, 10);
     gtk_box_pack_start(GTK_BOX(boxin), but_about, 1, 1, 0);
-    gtk_box_pack_start(GTK_BOX(boxin), label3, 0, 0, 120);
+    gtk_box_pack_start(GTK_BOX(boxin), label3, 0, 0, 100);
     gtk_box_pack_start(GTK_BOX(bigbox), doneright, 1, 1, 5);
-    gtk_box_pack_start(GTK_BOX(doneright), label2, 1, 1, 250);
+    gtk_box_pack_start(GTK_BOX(doneright), label2, 1, 1, 240);
     gtk_box_pack_start(GTK_BOX(doneright), but_exit, 1, 1, 0);
     //gtk_widget_set_name(logo, "logo");
     gtk_widget_set_name(but_about, "about");
     gtk_widget_set_name(but_exit, "exit");
     gtk_widget_set_name(main_menu, "main");
+    g_signal_connect(but_exit, "clicked", G_CALLBACK(gtk_main_quit), NULL);
     g_signal_connect(but_start, "clicked", G_CALLBACK(named), first);
     g_signal_connect(but_start, "clicked", G_CALLBACK(widget_destroy), bigbox);
+    g_signal_connect(credit, "clicked", G_CALLBACK(show_credit), NULL);
     gtk_container_add(GTK_CONTAINER(main_menu), first);
     gtk_window_set_position(GTK_WINDOW(main_menu), GTK_WIN_POS_CENTER);
     gtk_container_set_border_width (GTK_CONTAINER (main_menu), 10);
@@ -621,10 +673,10 @@ int main (int argc, char *argv[])
                                      " #main {\n"
                                      "   background-image: url('background.jpg');\n"
                                      "   background-size: 800px 600px;\n"
+                                     "   background-repeat: no-repeat;\n"
                                      "}\n"
                                      "#about {\n"
-                                     "   color: green;\n"
-                                     "   background-color: black;\n"
+                                     "   background-color: green;\n"
                                      "}\n"
                                      "#exit {\n"
                                      "   color: blue;\n"
@@ -636,4 +688,17 @@ int main (int argc, char *argv[])
     gtk_widget_show_all(main_menu);
     gtk_main ();
     return 0;
+}
+
+int show_credit()
+{
+    GtkWidget *window;
+    GtkImage *pic_credit;
+    window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title (GTK_WINDOW (window), "CREDIT");
+    pic_credit = gtk_image_new_from_file("credit.jpg");
+    gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
+    gtk_container_add(GTK_CONTAINER(window), pic_credit);
+    gtk_window_set_resizable(GTK_WINDOW(window), FALSE);
+    gtk_widget_show_all(window);
 }
